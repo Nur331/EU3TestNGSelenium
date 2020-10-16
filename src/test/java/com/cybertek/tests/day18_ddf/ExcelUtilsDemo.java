547cb98b8bbc1cfd61d37dff18ee9da4c5d3a1e8
Alinur Yaser <alinuer.jasean@gmail.com>
2020-10-17 00:24:49 +0200

    testng-runner.xml

---
+++ src/test/java/com/cybertek/tests/day18_ddf/ExcelUtilsDemo.java
@@ -0,0 +1,47 @@
+package com.cybertek.tests.day18_ddf;
+
+import com.cybertek.utilities.ExcelUtil;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.Map;
+
+public class ExcelUtilsDemo {
+ // no need t extends TestBase
+
+ @Test
+ public void readExcelFile(){
+
+ // create an object from ExcelUtil
+ // it accepts two arguments
+ // Argument 1 : location of the file (path)
+ // Argument 2 : sheet that we want to open(sheetName)
+
+ExcelUtil qa3short=new ExcelUtil("src/test/resources/Vytracktestdata.xlsx","QA3-short");
+
+// how  many rows in the sheet
+     System.out.println("qa3short.rowCount() = " + qa3short.rowCount());
+
+//how many columns in the sheet
+     System.out.println("qa3short.columnCount() = " + qa3short.columnCount());
+
+ // get all column names
+     System.out.println("qa3short.getColumnsNames() = " + qa3short.getColumnsNames());
+
+ // get all data in list of maps
+     List<Map<String, String>> dataList = qa3short.getDataList();
+
+     for (Map<String, String> onerow : dataList) {
+         System.out.println(onerow);
+
+     }
+
+     // get Nona carroll as a value
+     System.out.println("dataList.get(2) = " + dataList.get(2).get("firstname"));
+
+
+
+
+ }
+
+}
